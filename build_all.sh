6e521255b9562264afb2342c848c774ab30c4e30
Isaac Kiptanui <ikiptanui@powergen-re.com>
2021-04-10 17:48:17 +0300

    build executable file

--- build_all.sh
+++ build_all.sh
@@ -2,8 +2,8 @@
 
 pushd .
 HERE="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-echo $HERE
-cd $HERE
+# echo $HERE
+# cd $HERE
 go get ./...
-./go_multi_arch_build.sh github.com/eloylp/go-telegram-uploader $HERE/build 'windows/amd64|linux/amd64|linux/arm/5'
+./go_multi_arch_build.sh github.com/techieIsaac/go-telegram-uploader ./build 'windows/amd64|linux/amd64|linux/arm/5'
 popd
