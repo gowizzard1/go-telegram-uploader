698d832583d66df9ce83bd42bc75c9378ed2bddd
Isaac Kiptanui <ikiptanui@powergen-re.com>
2021-04-10 17:51:59 +0300

    build executable file

--- watcher/watcher_linux.go
+++ watcher/watcher_linux.go
@@ -1,14 +1,16 @@
 package watcher
 
 import (
+	"powergen/go-telegram-uploader/config"
+
 	"github.com/eloylp/go-telegram-uploader/fails"
 	"github.com/eloylp/go-telegram-uploader/handler"
 	"github.com/rjeczalik/notify"
 )
 
-func Watcher(initialPath string) {
+func Watcher(conf config.Configs) {
 	events := make(chan notify.EventInfo, 1)
-	err := notify.Watch(initialPath, events, notify.InCloseWrite)
+	err := notify.Watch(conf.FolderToScan, events, notify.InCloseWrite)
 	fails.FailIfError(err)
 	startWatcher(events, handler.ProcessFile)
 }
