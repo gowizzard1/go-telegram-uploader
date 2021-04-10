26ba427199f34737eb4173259d27730bc14998d2
Isaac Kiptanui <ikiptanui@powergen-re.com>
2021-04-10 17:55:32 +0300

    build executable file

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"log"
 	"powergen/go-telegram-uploader/config"
 	"powergen/go-telegram-uploader/watcher"
 
@@ -8,6 +9,7 @@ import (
 )
 
 func main() {
+	log.Printf("Starting app. This will be terminated by CR+LF\r")
 	godotenv.Load()
 	conf := config.BuildConfigs()
 	watcher.Watcher(conf)
