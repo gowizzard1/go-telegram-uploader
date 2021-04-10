91f281ca01e83e00714c9df43703984f0a260046
Isaac Kiptanui <ikiptanui@powergen-re.com>
2021-04-10 20:41:52 +0300

    build executable file

--- main.go
+++ main.go
@@ -6,10 +6,13 @@ import (
 	"powergen/go-telegram-uploader/watcher"
 
 	"github.com/joho/godotenv"
+	log "github.com/visionmedia/go-cli-log"
 )
 
 func main() {
-	fmt.Println("haha")
+	fmt.Println()
+	log.Info("unpack", "tarball to node_modules/express")
+	fmt.Println()
 	godotenv.Load()
 	conf := config.BuildConfigs()
 	watcher.Watcher(conf)
