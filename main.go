cb16503fdaea4b2a2e7f5925419d54ff61885a14
Isaac Kiptanui <ikiptanui@powergen-re.com>
2021-04-12 11:59:47 +0300

    commit

--- main.go
+++ main.go
@@ -3,19 +3,14 @@ package main
 import (
 	"fmt"
 	"os"
-	"os/exec"
 	"powergen/go-telegram-uploader/config"
 	"powergen/go-telegram-uploader/watcher"
-	"strings"
 
 	"github.com/joho/godotenv"
 )
 
 func main() {
-	cmd := exec.Command("pass")
-	cmd.Stdin = strings.NewReader("Secret pass!\n")
-	cmd.Stdout = os.Stdout
-	_ = cmd.Run()
+	os.Exit(500)
 	fmt.Println()
 	fmt.Println()
 	godotenv.Load()
