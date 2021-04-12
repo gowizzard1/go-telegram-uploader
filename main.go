92c608b60240a46cb216db195e8d4b45b7edf405
Isaac Kiptanui <ikiptanui@powergen-re.com>
2021-04-12 11:56:23 +0300

    commit

--- main.go
+++ main.go
@@ -6,12 +6,10 @@ import (
 	"powergen/go-telegram-uploader/watcher"
 
 	"github.com/joho/godotenv"
-	log "github.com/visionmedia/go-cli-log"
 )
 
 func main() {
 	fmt.Println()
-	log.Info("unpack", "tarball to node_modules/express")
 	fmt.Println()
 	godotenv.Load()
 	conf := config.BuildConfigs()
